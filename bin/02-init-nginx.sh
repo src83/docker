6ae5cdc5f85137559c5e10bc1011aaac7c6b48be
Roman Staroseltsev <src83@mail.ru>
2023-05-19 10:59:35 +0300

    Simplified nginx setup for subdomain support

--- bin/02-init-nginx.sh
+++ bin/02-init-nginx.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-cp .env.blank .env
-cp blank.Makefile Makefile
-cp containers/mysql/database.blank.env containers/mysql/database.env
+if [ $# -eq 0 ]; then
+    cp .env.blank .env
+    cp blank.Makefile Makefile
+    cp containers/mysql/database.blank.env containers/mysql/database.env
+
+    mkdir -p containers/nginx/configs
+    cp containers/nginx/examples/1-main/domain/local.conf containers/nginx/configs
+    cp containers/nginx/examples/2-parts/nginx-common-begin.conf containers/nginx/configs
+    exit 1
+fi
+
+
+if [ "$1" = "-s" ]; then
+    cp containers/nginx/examples/1-main/subdomain/local.conf containers/nginx/configs
+    cp containers/nginx/examples/2-parts/nginx-common-begin.conf containers/nginx/configs
+    cp containers/nginx/examples/3-snippets/mobile_subdomain.conf containers/nginx/configs
+else
+    echo "Invalid parameter."
+    exit 1
+fi
---
+++ init_ext_nginx.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+bash ./bin/02-init-nginx.sh -s
+
